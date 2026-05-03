7356303bd404c695f5cbc21e9fdfd2ce0ca08c7f
Marcelo Ernesto Rodriguez Sánchez <2024020080@cvirtualuees.edu.sv>
2026-05-03 09:14:56 -0600

    frontend improve

--- backend/src/frontend/src/components/ui/Badge.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const Badge = ({ estado, children }) => {
-  const clase = estado ? `badge badge-${estado}` : "badge";
-  return <span className={clase}>{children}</span>;
-};
-
-export default Badge;
